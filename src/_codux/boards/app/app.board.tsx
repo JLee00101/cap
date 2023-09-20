6db11f21777e0efc45f0964050a5195064ca975c
JLee00101 <leejc01@hotmail.com>
2023-09-21 09:03:01 +1000

    frontPage_iteration1

--- src/_codux/boards/app/app.board.tsx
+++ src/_codux/boards/app/app.board.tsx
@@ -1,7 +1,12 @@
 import { createBoard } from '@wixc3/react-board';
 import App from '../../../App';
+import { MemoryRouter } from 'react-router-dom';
 
 export default createBoard({
     name: 'App',
-    Board: () => null,
+    Board: () => (
+        <MemoryRouter>
+            <App />
+        </MemoryRouter>
+    ),
 });
---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,29 @@
+import classNames from 'classnames';
+import styles from './navbar.module.scss';
+import login from '../login/login';
+import { Link} from 'react-router-dom';
+
+export interface NavbarProps {
+    className?: string;
+}
+
+/**
+ * This component was created using Codux's Default new component template.
+ * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
+ */
+export const Navbar = ({ className }: NavbarProps) => {
+    return (
+        <nav className={styles.navBar}>
+                <li>
+                <Link to={'/'}>Home</Link>
+            </li>
+            {/*  <Link to={'/'}>
+                <h3 className={styles.navElement}>Home</h3>
+            </Link>      
+                <h3 className={styles.navElement}> Ongoing Projects</h3>
+                <h3 className={styles.navElement}> Upcoming Projects</h3>
+                <h3 className={styles.navElement}> Staff</h3>
+    <h3 className={styles.navElement}> Profile</h3> */}
+        </nav>
+    );
+};
